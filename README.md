5159eaa55496e81cbde4bb90da44abfd564dc54e
phonghp69 <phongdthe140091@gmail.com>
2022-03-08 16:19:35 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-3auhhs-rwdzrb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-3auhhs-rwdzrb)
---
+++ src/pages/homePage/homepage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+function homePage() {
+  return (
+    <h2>
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+      Lorem Ipsum has been the industry's standard dummy text ever since the
+      1500s, when an unknown printer took a galley of type and scrambled it to
+      make a type specimen book
+    </h2>
+  );
+}
+ReactDOM.render(<homePage />, document.getElementById('root'));
